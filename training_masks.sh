4771069d0a784ce4a99ad07688ea977f483d18be
kaeppelm <kaeppelm@informatik.uni-freiburg.de>
2021-11-24 01:54:56 +0100

    changed batch size

---
+++ training_masks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -p alldlc_gpu-rtx2080 # partition (queue)
+#SBATCH --mem 128G # memory pool for each core (4GB)
+#SBATCH -t 0-00:10 # time (D-HH:MM)
+#SBATCH -c 8 # number of cores
+#SBATCH --gres=gpu:2
+#SBATCH -D /work/dlclarge1/kaeppelm-cleargrasp
+#SBATCH -o %x.%N.%j.out # STDOUT  
+# #SBATCH -e %x.%N.%j.err # STDERR 
+#SBATCH -J cleargrasp_training_masks # sets the job name. If not specified, the file name will be used as job name
+#SBATCH --mail-type=END,FAIL # (recive mails about end and timeouts/crashes of your job)
+
+# Print some information about the job to STDOUT
+cd /work/dlclarge1/kaeppelm-cleargrasp/
+echo "Workingdir: $PWD";
+echo "Started at $(date)";
+echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
+
+# Job to perform
+source ~/.bashrc
+conda activate cleargrasp
+cd /home/kaeppelm/depth-via-interaction/cleargrasp/pytorch_networks/masks
+srun python train.py -c config/config.yaml
+
+# Print some Information about the end-time to STDOUT
+echo "DONE";
+echo "Finished at $(date)";
